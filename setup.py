4654925459b5a038211c0fbb8a13b9e40fd3512a
Peter Odding <peter@peterodding.com>
2016-08-08 23:12:00 +0200

    Minor improvements to setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """Setup script for the `humanfriendly` package."""
 
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: February 20, 2016
+# Last Change: August 4, 2016
 # URL: https://humanfriendly.readthedocs.org
 
 # Standard library modules.
@@ -16,19 +16,22 @@ import sys
 from setuptools import find_packages, setup
 
 
-def get_contents(filename):
+def get_contents(*args):
     """Get the contents of a file relative to the source distribution directory."""
-    directory = os.path.dirname(os.path.abspath(__file__))
-    pathname = os.path.join(directory, filename)
-    with codecs.open(pathname, 'r', 'utf-8') as handle:
+    with codecs.open(get_absolute_path(*args), 'r', 'utf-8') as handle:
         return handle.read()
 
 
-def get_version(filename):
+def get_version(*args):
     """Extract the version number from a Python module."""
-    contents = get_contents(filename)
+    contents = get_contents(*args)
     metadata = dict(re.findall('__([a-z]+)__ = [\'"]([^\'"]+)', contents))
     return metadata['version']
+
+
+def get_absolute_path(*args):
+    """Transform relative pathnames into absolute pathnames."""
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
 
 
 def have_environment_marker_support():
@@ -59,7 +62,7 @@ if have_environment_marker_support():
 
 setup(
     name='humanfriendly',
-    version=get_version('humanfriendly/__init__.py'),
+    version=get_version('humanfriendly', '__init__.py'),
     description="Human friendly output for text interfaces using Python",
     long_description=get_contents('README.rst'),
     url='https://humanfriendly.readthedocs.org',
