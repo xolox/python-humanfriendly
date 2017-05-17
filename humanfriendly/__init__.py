5e2b866abdd7742e1690e882b78623adc4ae2a5e
Peter Odding <peter@peterodding.com>
2017-05-18 00:55:16 +0200

    Release 3.2: Auto-encode terminal output to avoid encoding errors

--- humanfriendly/__init__.py
+++ humanfriendly/__init__.py
@@ -1,7 +1,7 @@
 # Human friendly input/output in Python.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: May 6, 2017
+# Last Change: May 18, 2017
 # URL: https://humanfriendly.readthedocs.io
 
 """The main module of the `humanfriendly` package."""
@@ -39,7 +39,7 @@ from humanfriendly.prompts import prompt_for_choice  # NOQA
 from humanfriendly.compat import is_string, monotonic
 
 # Semi-standard module versioning.
-__version__ = '3.1'
+__version__ = '3.2'
 
 # Spinners are redrawn at most this many seconds.
 minimum_spinner_interval = 0.2
