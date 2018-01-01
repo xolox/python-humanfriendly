8bf86544f41e668e46dcc06f2e6762ccf0be9eb2
Peter Odding <peter@peterodding.com>
2018-01-01 22:35:28 +0100

    Change Sphinx documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,4 @@ intersphinx_mapping = dict(
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'humanfriendlydoc'
+html_theme = 'nature'
