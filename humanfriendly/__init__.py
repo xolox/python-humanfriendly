028b7c2f9688bddf0a45b4ca285c8b43a5c5d8e7
Peter Odding <peter@peterodding.com>
2018-01-14 03:27:44 +0100

    Release 4.7: Support background colors and 256 color mode
    Related to issue 35 on the coloredlogs issue tracker:
    https://github.com/xolox/python-coloredlogs/issues/35

--- humanfriendly/__init__.py
+++ humanfriendly/__init__.py
@@ -1,7 +1,7 @@
 # Human friendly input/output in Python.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: January 4, 2018
+# Last Change: January 14, 2018
 # URL: https://humanfriendly.readthedocs.io
 
 """The main module of the `humanfriendly` package."""
@@ -39,7 +39,7 @@ from humanfriendly.prompts import prompt_for_choice  # NOQA
 from humanfriendly.compat import is_string, monotonic
 
 # Semi-standard module versioning.
-__version__ = '4.6'
+__version__ = '4.7'
 
 # Spinners are redrawn at most this many seconds.
 minimum_spinner_interval = 0.2
