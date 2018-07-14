xolox/python-humanfriendly

commit 4c2a37fd226308583ccc86f54a4f021feefeb9f3

Bug fixes for HTML to ANSI conversion*
HTML entities should of course be decoded and copied to the output
stream, but this wasn't actually happening because I had neglected to
define the handle_charref() and handle_entityref() methods (whose
definitions are so conveniently given in the documentation of the
HTMLParser class 😇).

humanfriendly/compat.py
@@ -1,7 +1,7 @@
 # Human friendly input/output in Python.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: July 13, 2018
+# Last Change: July 14, 2018
 # URL: https://humanfriendly.readthedocs.io
 
 """
@@ -51,6 +51,7 @@ __all__ = (
     'is_string',
     'is_unicode',
     'monotonic',
+    'unichr',
     'unicode',
     'unittest',
 )
@@ -58,6 +59,7 @@ __all__ = (
 try:
     # Python 2.
     unicode = unicode
+    unichr = unichr
     basestring = basestring
     interactive_prompt = raw_input
     from HTMLParser import HTMLParser
@@ -65,6 +67,7 @@ try:
 except (ImportError, NameError):
     # Python 3.
     unicode = str
+    unichr = chr
     basestring = str
     interactive_prompt = input
     from html.parser import HTMLParser

humanfriendly/terminal.py
@@ -18,6 +18,7 @@ escape sequences work.
 
 # Standard library modules.
 import codecs
+import htmlentitydefs
 import numbers
 import os
 import re
@@ -38,7 +39,7 @@ except ImportError:
 # Modules included in our package. We import find_meta_variables() here to
 # preserve backwards compatibility with older versions of humanfriendly where
 # that function was defined in this module.
-from humanfriendly.compat import HTMLParser, StringIO, coerce_string, is_unicode
+from humanfriendly.compat import HTMLParser, StringIO, coerce_string, is_unicode, unichr
 from humanfriendly.text import concatenate, format
 from humanfriendly.usage import find_meta_variables, format_usage  # NOQA
 
@@ -736,9 +737,17 @@ class HTMLConverter(HTMLParser):
         else:
             self.output.write(ANSI_RESET)
 
+    def handle_charref(self, value):
+        """
+        Process a decimal or hexadecimal numeric character reference.
+
+        :param value: The decimal or hexadecimal value (a string).
+        """
+        self.output.write(unichr(int(value[1:], 16) if value.startswith('x') else int(value)))
+
     def handle_data(self, data):
         """
-        Handle textual data.
+        Process textual data.
 
         :param data: The decoded text (a string).
         """
@@ -755,7 +764,7 @@ class HTMLConverter(HTMLParser):
 
     def handle_endtag(self, tag):
         """
-        Handle the end of an HTML tag.
+        Process the end of an HTML tag.
 
         :param tag: The name of the tag (a string).
         """
@@ -776,9 +785,17 @@ class HTMLConverter(HTMLParser):
             else:
                 self.emit_style(new_style)
 
+    def handle_entityref(self, name):
+        """
+        Process a named character reference.
+
+        :param name: The name of the character reference (a string).
+        """
+        self.output.write(unichr(htmlentitydefs.name2codepoint[name]))
+
     def handle_starttag(self, tag, attrs):
         """
-        Handle the start of an HTML tag.
+        Process the start of an HTML tag.
 
         :param tag: The name of the tag (a string).
         :param attrs: A list of tuples with two strings each.
@@ -789,18 +806,18 @@ class HTMLConverter(HTMLParser):
             # can render the link text before the URL (with the reasoning that
             # this is the most intuitive way to present a link in a plain text
             # interface).
-            self.link_url = next((v for n, v in attrs if n == 'href'), "")
-        elif tag in ('b', 'strong'):
+            self.link_url = next((v for n, v in attrs if n == 'href'), '')
+        elif tag == 'b' or tag == 'strong':
             self.push_styles(bold=True)
         elif tag == 'br':
             self.output.write('\n')
-        elif tag in ('code', 'pre'):
+        elif tag == 'code' or tag == 'pre':
             self.push_styles(color='yellow')
-        elif tag in ('del', 's'):
+        elif tag == 'del' or tag == 's':
             self.push_styles(strike_through=True)
-        elif tag in ('em', 'i'):
+        elif tag == 'em' or tag == 'i':
             self.push_styles(italic=True)
-        elif tag in ('ins', 'u'):
+        elif tag == 'ins' or tag == 'u':
             self.push_styles(underline=True)
         elif tag == 'span':
             styles = {}

humanfriendly/tests.py
@@ -872,6 +872,14 @@ class HumanFriendlyTestCase(TestCase):
         # Line breaks.
         assert html_to_ansi("Let's try some<br>line<br>breaks") == \
             "Let's try some\nline\nbreaks"
+        # Check that decimal entities are decoded.
+        assert html_to_ansi("&#38;") == "&"
+        # Check that named entities are decoded.
+        assert html_to_ansi("&amp;") == "&"
+        assert html_to_ansi("&gt;") == ">"
+        assert html_to_ansi("&lt;") == "<"
+        # Check that hexadecimal entities are decoded.
+        assert html_to_ansi("&#x26;") == "&"
 
     def test_generate_output(self):
         """Test the :func:`humanfriendly.terminal.output()` function."""

commit 69888298d2b031b040e222706b592ccd62194724

Restore Python 3 compatibility (broken in 4c2a37fd226)

humanfriendly/compat.py
@@ -51,6 +51,7 @@ __all__ = (
     'is_string',
     'is_unicode',
     'monotonic',
+    'name2codepoint',
     'unichr',
     'unicode',
     'unittest',
@@ -64,6 +65,7 @@ try:
     interactive_prompt = raw_input
     from HTMLParser import HTMLParser
     from StringIO import StringIO
+    from htmlentitydefs import name2codepoint
 except (ImportError, NameError):
     # Python 3.
     unicode = str
@@ -72,6 +74,7 @@ except (ImportError, NameError):
     interactive_prompt = input
     from html.parser import HTMLParser
     from io import StringIO
+    from html.entities import name2codepoint
 
 try:
     # Python 3.3 and higher.

humanfriendly/terminal.py
@@ -18,7 +18,6 @@ escape sequences work.
 
 # Standard library modules.
 import codecs
-import htmlentitydefs
 import numbers
 import os
 import re
@@ -39,7 +38,7 @@ except ImportError:
 # Modules included in our package. We import find_meta_variables() here to
 # preserve backwards compatibility with older versions of humanfriendly where
 # that function was defined in this module.
-from humanfriendly.compat import HTMLParser, StringIO, coerce_string, is_unicode, unichr
+from humanfriendly.compat import HTMLParser, StringIO, coerce_string, name2codepoint, is_unicode, unichr
 from humanfriendly.text import concatenate, format
 from humanfriendly.usage import find_meta_variables, format_usage  # NOQA
 
@@ -791,7 +790,7 @@ class HTMLConverter(HTMLParser):
 
         :param name: The name of the character reference (a string).
         """
-        self.output.write(unichr(htmlentitydefs.name2codepoint[name]))
+        self.output.write(unichr(name2codepoint[name]))
 
     def handle_starttag(self, tag, attrs):
         """