3030450de13bc71e1aea57514e03736fade7020a
Hugo <hugovk@users.noreply.github.com>
2019-07-04 18:02:18 +0300

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
         'capturer >= 2.1',
         'coloredlogs >= 2.0',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 6 - Mature',
         'Environment :: Console',
@@ -121,7 +121,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
