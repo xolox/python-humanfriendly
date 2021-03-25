d95d308becaecb194d9fba2b0420bd88b3c11c60
Blaise Kal <blaisekal@gmail.com>
2021-03-25 11:40:13 +0100

    Declare parallel_read_safe=True in setup
    See https://www.sphinx-doc.org/en/master/extdev/index.html#extension-metadata

--- humanfriendly/sphinx.py
+++ humanfriendly/sphinx.py
@@ -250,6 +250,10 @@ def setup(app):
     enable_pypi_role(app)
     enable_special_methods(app)
     enable_usage_formatting(app)
+    
+    return {
+        "parallel_read_safe": True,
+    }
 
 
 def special_methods_callback(app, what, name, obj, skip, options):
